c948a7a51b46b620677e20b422beddd9b5e3d143
ottokafka <ottokafka@yahoo.com>
2018-05-23 20:17:54 +0800

    updated python code

--- README.md
+++ README.md
@@ -24,4 +24,34 @@ Once downloaded go to the directory in your terminal
 ![image](https://user-images.githubusercontent.com/21117852/40422712-c56a3ab2-5ec2-11e8-8fc8-484f8c96fc8e.png)
 
 
+To Run the python program
+
+```
+python3 dragonBallZ.py
+```
+
+![image](https://user-images.githubusercontent.com/21117852/40422751-daacd236-5ec2-11e8-9c56-88100563639f.png)
+
+
+You will need to change the download directory: example below -
+
+![image](https://user-images.githubusercontent.com/21117852/40423276-a4656ae2-5ec4-11e8-989c-34a594a208e3.png)
+
+
+How many video do you want to download if all type while != 291
+
+![image](https://user-images.githubusercontent.com/21117852/40423610-b381b62e-5ec5-11e8-86e5-9e42186db7df.png)
+
+
+
+Make sure to download these files with the terminal
+
+```
+pip3 install requests
+pip3 install bs4
+pip3 install re
+pip3 install shutil
+```
+
+![image](https://user-images.githubusercontent.com/21117852/40423709-edc8ec9e-5ec5-11e8-829e-b9a8cd70dd57.png)
 
--- dragonBallZ.py
+++ dragonBallZ.py
@@ -6,13 +6,13 @@ import shutil
 count = 1
 videoLinksList = []
 
-
+## This while loop we can change how many videos we want to download if only want first 3 episodes use 3
 while count != 3:
 
     ##  The first url dragonBall Z Episode 1
     url1 = "http://www.99tw.net/xplay/irmp408220.html?ep=" + str(count)
 
-    print(url1)
+    # print(url1)
 
     ## headers so the fucking website doesnt think we are a bot
     headers1 = {
@@ -22,7 +22,7 @@ while count != 3:
     res = requests.get(url1, headers=headers1, stream=True)
 
     ##  check if the status code is good
-    print("if you get 200 tots but if you get 403 or 404 its the fucken headers fault - anyway the result is " + str(
+    print("if you get 200 GREAT!! if 404 or 403 headers are bad and your result is: " + str(
         res.status_code))
 
     ## grab the html text
@@ -40,23 +40,27 @@ while count != 3:
         # add the match to a list
         videoLinksList.append(match)
 
-    ## Using the negative -1 means to grabs something from the end of the array
+    ## Using the negative -1 -1 means to grab something from the end of the list
     videoLink1 = videoLinksList[-1][-1]
-    print("Grabs link from the end of the List ")
     print(videoLink1)
-    print(videoLinksList)
+    # print(videoLinksList)
 
     ## Go to the new video link which as the pure video
     pureVideoDownload = requests.get(videoLink1, headers=headers1, stream=True)
     print(pureVideoDownload.status_code)
 
-    'Downloading video please wait '
+    print('----------Downloading video please wait --------------')
+
+    ## Here we need to change the download directory
+    ## Example '/Volumes/YOUR USER NAME/tvShows/DragonBallZ/'
+    ## Example '/Volumes/YOUR USER NAME/Documents'
     with open('/Volumes/2ndDrive/tvShows/DragonBallZ/' + 'DragonBallZ- ' + str(count) + '.mp4', 'wb') as out_file:
         shutil.copyfileobj(pureVideoDownload.raw, out_file)
         # del will get out of the current file so the program can continue
     del pureVideoDownload
 
+    print(str(count) + " Of 291")
+    print("------Grabbing the next video please wait-----------")
     count = count + 1
-    print(count)
 
 
